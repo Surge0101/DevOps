30f09ca204c4d6fe16b5833c3cb3c8b20fcca8ce
Brandon <Brandongoodman58@gmail.com>
2026-05-17 20:21:05 +0800

    Backend-app creation

---
+++ cdk/backend-app/src/main/java/com/bgapp/BgAppApplication.java
@@ -0,0 +1,12 @@
+package com.bgapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgAppApplication.class, args);
+    }
+}
---
+++ cdk/backend-app/src/test/java/com/bgapp/BgAppApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bgapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BgAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
